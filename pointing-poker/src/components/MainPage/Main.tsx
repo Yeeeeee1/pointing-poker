465208eca2ffb37076d34910a5a64bd57648a6b1
Danil <danil.rodionow13@gmail.com>
2021-09-03 20:16:51 +0300

    feat: main page layout

--- pointing-poker/src/components/MainPage/Main.tsx
+++ pointing-poker/src/components/MainPage/Main.tsx
@@ -1,5 +1,55 @@
 import React, { ReactElement } from "react";
+import { Button, Form, Col, Row } from "react-bootstrap";
+import "./mainPage.scss";
+import MainPageLogo from "../../shared/SVG/MainPageLogo";
 
-const MainPage = (): ReactElement => <div>Main Page</div>;
+const MainPage = (): ReactElement => (
+  <div className="mainPage">
+    <MainPageLogo />
+    <div className="mainPageSection">
+      <h2 className="mainPageTitle">Start your planning:</h2>
+      <div className="mainPageControlsWrapper">
+        <p>Create session: </p>
+        <Button
+          className="mainPageButton"
+          as="input"
+          type="button"
+          value="Start new game"
+        />
+      </div>
+    </div>
+    <div className="mainPageSection">
+      <h2 className="mainPageTitle">OR:</h2>
+      <p>
+        Connect to lobby by <a href="##">URL</a>:
+      </p>
+      <div className="mainPageControlsWrapper">
+        <Form>
+          <Row>
+            <Col xs="auto" className="my-1 mainPageCol">
+              <Form.Label
+                className="me-sm-2"
+                htmlFor="inlineFormCustomSelect"
+                visuallyHidden
+              >
+                Preference
+              </Form.Label>
+              <Form.Control
+                placeholder="Write lobby URL"
+                className="me-sm-2 mainPageInput"
+                id="inlineFormCustomSelect"
+              />
+            </Col>
+            <Col xs="auto" className="my-1 mainPageCol">
+              <Button className="mainPageButton" type="button">
+                Connect
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </div>
+    </div>
+  </div>
+);
 
 export default MainPage;
