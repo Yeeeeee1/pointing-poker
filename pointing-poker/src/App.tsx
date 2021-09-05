c6fdd2c5bba4329a9f90d1d9778f70ae61cfa8fe
Danil <danil.rodionow13@gmail.com>
2021-09-05 11:57:10 +0300

    feat: header layout

--- pointing-poker/src/App.tsx
+++ pointing-poker/src/App.tsx
@@ -1,10 +1,12 @@
 import React, { ReactElement } from "react";
 import { Route, Switch } from "react-router-dom";
 import routes from "./shared/globalVariables";
+import Header from "./components/Header/Header";
 
 function App(): ReactElement {
   return (
     <div>
+      <Header />
       <Switch>
         {routes.map(({ path, component, exact }) => (
           <Route key={path} path={path} component={component} exact={exact} />
