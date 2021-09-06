1c18627984993134d0c1a49bb5bc520fed7dde5b
Danil <danil.rodionow13@gmail.com>
2021-09-06 15:24:48 +0300

    feat: add *.d.ts file for correct import of css modules

--- pointing-poker/src/redux/store/reducers/userReducer.ts
+++ pointing-poker/src/redux/store/reducers/userReducer.ts
@@ -1,20 +1,22 @@
-import { IUserState, UserAction, UserActionType } from "../../types/user"
+import { IUserState, UserAction, UserActionType } from "../../types/user";
 
 const initialState: IUserState = {
   users: [],
   loading: false,
   cards: [],
-}
+};
 
-export const userReducer = (state = initialState, action: UserAction): IUserState => {
+const userReducer = (state = initialState, action: UserAction): IUserState => {
   switch (action.type) {
     case UserActionType.GET_USERS:
-      return {users: action.payload, loading: true, cards: []}
+      return { users: action.payload, loading: true, cards: [] };
     case UserActionType.GET_USERS_SUCCESS:
-      return {users: [], loading: action.payload, cards: []}
+      return { users: [], loading: action.payload, cards: [] };
     case UserActionType.GET_CARDS:
-      return {users: [], loading: true, cards: action.payload}
+      return { users: [], loading: true, cards: action.payload };
     default:
       return state;
   }
-}
+};
+
+export default userReducer;
