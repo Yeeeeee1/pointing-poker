bc55c758b553c8c8c4bd4c714ab43cf19f1f9c27
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2021-09-15 08:49:26 +0300

    feat: game page layout Issues

--- pointing-poker/src/components/Pages/GamePage/GamePage.tsx
+++ pointing-poker/src/components/Pages/GamePage/GamePage.tsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { FC } from "react";
 
-const GamePage = () => <div>Game Page</div>;
+const GamePage: FC = () => {
+  return (
+    <div>Game Page</div>
+  );
+};
 
 export default GamePage;
